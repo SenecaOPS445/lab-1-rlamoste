0a01599eb8b68f52916cf4604c27ba454ce670a0
Renante Lamoste <renante.lamoste1@gmail.com>
2024-05-15 23:55:25 -0400

    Individual message or note.

---
+++ lab1c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+
+
+# Create variables for name and age
+name = "Isaac"
+age = 72
+
+# Print out the desired message
+print(name + " is " + str(age) + " years old!")
---
+++ lab1d.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# Define the integer values
+x = 10
+y = 2
+z = 5
+
+# Calculate the result
+result = x + y * z
+
+# Print the output
+print(str(x) + " + " + str(y) + " * " + str(z) + " = " + str(result))
